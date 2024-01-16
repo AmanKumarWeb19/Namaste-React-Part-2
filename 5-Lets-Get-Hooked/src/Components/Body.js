7eaba400b41e1f28e4a69bc9e259269339ae8105
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-01-16 08:36:26 +0530

    start filter part

--- 5-Lets-Get-Hooked/src/Components/Body.js
+++ 5-Lets-Get-Hooked/src/Components/Body.js
@@ -4,7 +4,16 @@ import RestuatantCard from "./RestuatantCard";
 const Body = () => {
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log("button clicked");
+          }}
+        >
+          Top Rated Restuarant
+        </button>
+      </div>
       <div className="res-container">
         {resList.map((restro) => (
           <RestuatantCard key={restro.info.id} resData={restro} />
