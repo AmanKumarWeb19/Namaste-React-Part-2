0de16c27857c97dcbf433d20f7e279f42786df80
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-01-16 09:48:29 +0530

    filter part using useState

--- 5-Lets-Get-Hooked/src/Components/Body.js
+++ 5-Lets-Get-Hooked/src/Components/Body.js
@@ -1,21 +1,38 @@
 import resList from "../utils/mockData";
 import RestuatantCard from "./RestuatantCard";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurant, setListOfResturant] = useState(resList);
+
   return (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
-            console.log("button clicked");
+            let filterRestro = listOfRestaurant.filter(
+              (res) => res.info.avgRating > 4
+            );
+            setListOfResturant(filterRestro);
           }}
         >
           Top Rated Restuarant
         </button>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            let filterRestro = listOfRestaurant.filter(
+              (res) => res.info.avgRating < 4
+            );
+            setListOfResturant(filterRestro);
+          }}
+        >
+          Less Rated Restuarant
+        </button>
       </div>
       <div className="res-container">
-        {resList.map((restro) => (
+        {listOfRestaurant.map((restro) => (
           <RestuatantCard key={restro.info.id} resData={restro} />
         ))}
       </div>
--- 5-Lets-Get-Hooked/src/Components/RestuatantCard.js
+++ 5-Lets-Get-Hooked/src/Components/RestuatantCard.js
@@ -20,7 +20,7 @@ const RestuatantCard = (props) => {
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} stars</h4>
-      <h4>{costForTwo}</h4>
+      <h4>₹{costForTwo} for Two</h4>
       <h4>{deliveryTime} minutes</h4>
     </div>
   );
