ba7e22a5870ac71bcd603e3dd45a4f30c20108cf
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-01-16 14:55:44 +0530

    update

--- 6-Exploring-The-World/App.js
+++ 6-Exploring-The-World/App.js
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import Header from "./src/components/Header";
 import Body from "./src/components/Body";
 
-const AppLayout = () => {
+const AppLayOut = () => {
   return (
-    <div>
+    <div className="App">
       <Header />
       <Body />
     </div>
   );
 };
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(AppLayout);
+root.render(<AppLayOut />);
