c22577f83acdcc3e25470c579e4f9b9408b50d2c
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-01-17 14:12:43 +0530

    login or logout

--- 6-Exploring-The-World/src/components/Header.js
+++ 6-Exploring-The-World/src/components/Header.js
@@ -1,6 +1,8 @@
 import { LOGO_URL } from "../utils/constant";
+import { useState } from "react";
 
 const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
   return (
     <div className="header">
       <div className="logoContainer">
@@ -12,6 +14,16 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="header-login-btn"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("LogOut")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
