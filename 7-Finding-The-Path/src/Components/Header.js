81dc8f161196d236855f009253dedefb6ec159ec
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-01-19 12:49:37 +0530

    link to page

--- 7-Finding-The-Path/src/Components/Header.js
+++ 7-Finding-The-Path/src/Components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [btnNameReact, setBtnNameReact] = useState("Login");
@@ -7,14 +8,24 @@ const Header = () => {
   return (
     <div className="header">
       <div className="logoContainer">
-        <img className="logo" src={LOGO_URL} alt="logo" />
+        <Link to="/">
+          <img className="logo" src={LOGO_URL} alt="logo" />
+        </Link>
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/carrt">Cart</Link>
+          </li>
           <button
             className="header-login-btn"
             onClick={() =>
