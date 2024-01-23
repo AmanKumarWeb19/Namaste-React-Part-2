a112b73879e6c74620aa105194fcffb26e4058e0
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-01-23 09:57:06 +0530

    update

--- 7-Finding-The-Path/src/Components/Body.js
+++ 7-Finding-The-Path/src/Components/Body.js
@@ -13,7 +13,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.93519229&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     // console.log(
