11f0c4230c30d0f774b4c151bcf5f0a3df3ff042
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-01-24 10:59:28 +0530

    resinfo usetate and fetch dynamic api

--- 8-Lets-get-Classy/src/components/RestaurantMenu.js
+++ 8-Lets-get-Classy/src/components/RestaurantMenu.js
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+
 const RestaurantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
+
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=425&submitAction=ENTER"
+    );
+
+    const json = await data.json();
+
+    console.log(json);
+
+    setResInfo(json.data);
+  };
+
+  if (resInfo === null) {
+    return <Shimmer />;
+  }
+
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.cards[0]?.card?.card?.info;
+
   return (
     <div className="menu">
-      <h1>Name of the Restaurant</h1>
+      <h1>{name}</h1>
+      <h3>
+        {cuisines.join(", ")} - {costForTwoMessage}
+      </h3>
       <h2>Menu</h2>
       <ul>
         <li>Biryani</li>
