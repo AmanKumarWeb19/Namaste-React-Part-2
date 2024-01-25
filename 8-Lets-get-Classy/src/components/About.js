6e3bd1a4394adcd20761aeb49213f06ae0c60b81
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-01-25 09:50:27 +0530

    practice for class component

--- 8-Lets-get-Classy/src/components/About.js
+++ 8-Lets-get-Classy/src/components/About.js
@@ -1,9 +1,13 @@
 import User from "./User";
+import UserClass from "./UserClass";
 const About = () => {
   return (
     <div>
       <h1>About</h1>
+      <h3>This is Namaste React Series</h3>
+
       <User />
+      <UserClass />
     </div>
   );
 };
---
+++ 8-Lets-get-Classy/src/components/UserClass.js
@@ -0,0 +1,13 @@
+import React from "react";
+class UserClass extends React.Component {
+  render() {
+    return (
+      <div className="user-card">
+        <h2>Name: Aman Kumar</h2>
+        <h3>Location: Patna</h3>
+        <h4>Contact: 6202375899</h4>
+      </div>
+    );
+  }
+}
+export default UserClass;
