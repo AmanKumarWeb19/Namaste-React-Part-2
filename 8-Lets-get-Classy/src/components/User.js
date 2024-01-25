5c6cef6606c290af2e6b0543a0d6fa99574b7fff
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-01-25 10:49:19 +0530

    do state variable and check

--- 8-Lets-get-Classy/src/components/User.js
+++ 8-Lets-get-Classy/src/components/User.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
+
 const User = ({ name }) => {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(1);
   return (
     <div className="user-card">
+      <h1>Count :- {count}</h1>
+      <h1>Count :- {count2}</h1>
       <h2>Name: {name}</h2>
       <h3>Location: Patna</h3>
       <h4>Contact: 6202375899</h4>
--- 8-Lets-get-Classy/src/components/UserClass.js
+++ 8-Lets-get-Classy/src/components/UserClass.js
@@ -2,11 +2,19 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0,
+      count2: 2,
+    };
   }
   render() {
     const { name, location, contact } = this.props;
+    const { count, count2  } = this.state;
     return (
       <div className="user-card">
+        <h1>CountClass:- {count}</h1>
+        <h1>CountClass2:- {count2}</h1>
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: {contact}</h4>
