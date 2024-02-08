d84dedcf349ef075127ec816762a1354f30913d1
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-02-08 09:54:10 +0530

    useRestaurantMenu hook as a custom hook

--- 9-Optimising-our-App/src/components/RestaurantMenu.js
+++ 9-Optimising-our-App/src/components/RestaurantMenu.js
@@ -1,25 +1,11 @@
-import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { MENU_API } from "../utils/constant";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
   const { resId } = useParams();
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
-  const fetchMenu = async () => {
-    const data = await fetch(MENU_API + resId);
-
-    const json = await data.json();
-
-    console.log(json);
-
-    setResInfo(json.data);
-  };
+  const resInfo = useRestaurantMenu(resId);
 
   if (resInfo === null) {
     return <Shimmer />;
