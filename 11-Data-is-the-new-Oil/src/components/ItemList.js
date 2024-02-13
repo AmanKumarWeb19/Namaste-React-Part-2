56d03dc2a3f8ed95e32109bff44759c008012120
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-02-13 12:50:22 +0530

    add css

--- 11-Data-is-the-new-Oil/src/components/ItemList.js
+++ 11-Data-is-the-new-Oil/src/components/ItemList.js
@@ -7,19 +7,28 @@ const ItemList = ({ items }) => {
       {items.map((item) => (
         <div
           key={item.card.info.id}
-          className="p-2 m-2 border-gray-200 border-b-2 text-left"
+          className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between"
         >
-          <img src={CDN_URL + item.card.info.imageId} className="w-14" />
-          <div className="py-2">
-            <span>{item.card.info.name}</span>
-            <span>
-              - ₹
-              {item.card.info.price
-                ? item.card.info.price / 100
-                : item.card.info.defaultPrice / 100}
-            </span>
+          <div className="w-9/12">
+            <div className="py-2">
+              <span>{item.card.info.name}</span>
+              <span>
+                - ₹
+                {item.card.info.price
+                  ? item.card.info.price / 100
+                  : item.card.info.defaultPrice / 100}
+              </span>
+            </div>
+            <p className="text-xs">{item.card.info.description}</p>
           </div>
-          <p className="text-xs">{item.card.info.description}</p>
+          <div className="w-3/12 p-4">
+            <div className="absolute">
+              <button className="p-2 bg-green-300 text-black m-auto shadow-lg">
+                Add +
+              </button>
+            </div>
+            <img src={CDN_URL + item.card.info.imageId} className="w-full" />
+          </div>
         </div>
       ))}
     </div>
