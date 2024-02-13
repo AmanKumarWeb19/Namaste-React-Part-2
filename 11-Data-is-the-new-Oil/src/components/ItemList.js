f9b33163982e48094f8ba873dcffaca1396585de
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-02-13 10:57:52 +0530

    add list

--- 11-Data-is-the-new-Oil/src/components/ItemList.js
+++ 11-Data-is-the-new-Oil/src/components/ItemList.js
@@ -1,6 +1,28 @@
-const ItemList=()=>{
+import { CDN_URL } from "../utils/constant";
+
+const ItemList = ({ items }) => {
+  console.log({ items: items });
   return (
-    <div></div>
-  )
-}
-export default ItemList
+    <div>
+      {items.map((item) => (
+        <div
+          key={item.card.info.id}
+          className="p-2 m-2 border-gray-200 border-b-2 text-left"
+        >
+          <img src={CDN_URL + item.card.info.imageId} className="w-14" />
+          <div className="py-2">
+            <span>{item.card.info.name}</span>
+            <span>
+              - ₹
+              {item.card.info.price
+                ? item.card.info.price / 100
+                : item.card.info.defaultPrice / 100}
+            </span>
+          </div>
+          <p className="text-xs">{item.card.info.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default ItemList;
--- 11-Data-is-the-new-Oil/src/components/RestaurantCategory.js
+++ 11-Data-is-the-new-Oil/src/components/RestaurantCategory.js
@@ -1,15 +1,23 @@
+import ItemList from "./ItemList";
+
 const RestaurantCategory = ({ data }) => {
   console.log({ categorydata: data });
   return (
     <div>
-      {/* Header */}
-      <div className="flex justify-between w-6/12 mx-auto my-4 p-4 shadow-lg rounded-lg">
-        <span className="font-bold text-gray-500 text-lg">
-          {data.title} ({data.itemCards.length})
-        </span>
-        <span>🔽</span>
+      <div className="w-6/12 mx-auto my-4 p-4 shadow-lg rounded-lg">
+        {/* Header */}
+
+        <div className="flex justify-between">
+          <span className="font-bold text-gray-500 text-lg">
+            {data.title} ({data.itemCards.length})
+          </span>
+          <span>🔽</span>
+        </div>
+
+        {/* Accrodion Body */}
+
+        <ItemList items={data.itemCards} />
       </div>
-      {/* Accrodion Body */}
     </div>
   );
 };
