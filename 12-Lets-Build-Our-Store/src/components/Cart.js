b75a460df952148ed0ded199448d0bc1fb23632e
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-02-14 14:55:14 +0530

    add

--- 12-Lets-Build-Our-Store/src/components/Cart.js
+++ 12-Lets-Build-Our-Store/src/components/Cart.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ItemList from "./ItemList";
+import { clearCart } from "../utils/cartSlice";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const cartItems = useSelector((store) => store.cart.items);
+  console.log(cartItems);
+
+  const dispatch = useDispatch();
+
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+  return (
+    <div className="text-center m-4 p-4">
+      <h1 className="font-bold text-2xl">CART</h1>
+      <div className="w-6/12 m-auto">
+        <button
+          className="bg-black font-bold rounded-lg text-white m-2 p-2"
+          onClick={handleClearCart}
+        >
+          Clear Cart
+        </button>
+
+        {cartItems.length === 0 && (
+          <h1>Cart is Empty. Please Add Items to the cart !!</h1>
+        )}
+        <ItemList items={cartItems} />
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
