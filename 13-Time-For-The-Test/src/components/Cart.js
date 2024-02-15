3d11865e551e971e4038c769fc7931f5b431515e
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-02-15 12:26:53 +0530

    clear cart

--- 13-Time-For-The-Test/src/components/Cart.js
+++ 13-Time-For-The-Test/src/components/Cart.js
@@ -1,14 +1,26 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ItemList from "./ItemList";
+import { clearCart } from "../utils/cartSlice";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
+  const dispatch = useDispatch();
+
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
 
   return (
     <div className="text-center m-4 p-4">
       <h1 className="text-2xl font-bold">Cart</h1>
-      <div>
+      <div className="w-6/12 m-auto">
+        <button
+          className="m-2 p-2 bg-black text-white rounded-lg"
+          onClick={handleClearCart}
+        >
+          Clear Cart
+        </button>
         <ItemList items={cartItems} />
       </div>
     </div>
