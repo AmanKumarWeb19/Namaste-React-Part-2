0ad53e3fa763e068b33630bc3dde249843d805a7
AmanKumarWeb19 <akgupta0524@gmail.com>
2024-02-19 08:53:20 +0530

    change test to it

--- 13-Time-For-The-Test/src/components/__tests__/Contact.test.js
+++ 13-Time-For-The-Test/src/components/__tests__/Contact.test.js
@@ -3,25 +3,25 @@ import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
 describe("Contact us Page Test Case", () => {
-  test("should Contatct page Render", () => {
+  it("should Contatct page Render", () => {
     render(<Contact />);
     const heading = screen.getByRole("heading");
     expect(heading).toBeInTheDocument();
   });
 
-  test("should load Button inside the Contact Component", () => {
+  it("should load Button inside the Contact Component", () => {
     render(<Contact />);
     const button = screen.getByRole("button");
     expect(button).toBeInTheDocument();
   });
 
-  test("should load input name inside the Contact Component", () => {
+  it("should load input name inside the Contact Component", () => {
     render(<Contact />);
     const input = screen.getByPlaceholderText("name");
     expect(input).toBeInTheDocument();
   });
 
-  test("should load 2 input box inside contact componnet", () => {
+  it("should load 2 input box inside contact componnet", () => {
     render(<Contact />);
     const inputBoxes = screen.getAllByRole("textbox");
     expect(inputBoxes.length).toBe(2);
